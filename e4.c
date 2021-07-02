d097a1c17d48a05833a03badcd856747a7e00925
s1280049 <s1280049@gmail.com>
2021-07-02 11:21:07 +0900

    Won/lost message added

--- e4.c
+++ e4.c
@@ -13,5 +13,11 @@ diceB = rand() % 6 +1;
 printf("Die 1: %d\n",diceB);
 printf("Total value: %d\n",diceA+diceB);
 
+if (diceA+diceB > 7)
+{
+printf("You won!\n");
+}
+else printf("You lost!\n");
+
 return 0;
 }
